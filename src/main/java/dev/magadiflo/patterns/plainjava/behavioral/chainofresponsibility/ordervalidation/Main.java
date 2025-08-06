1448997a9b3cff1e19dc6c8570157bec8ab479a1
Martín Díaz <magadiflo@gmail.com>
2025-08-06 18:19:51 -0500

    Java - patrones de comportamiento - chain of responsibility - order validation - doc 08

--- src/main/java/dev/magadiflo/patterns/plainjava/behavioral/chainofresponsibility/ordervalidation/Main.java
+++ src/main/java/dev/magadiflo/patterns/plainjava/behavioral/chainofresponsibility/ordervalidation/Main.java
@@ -12,12 +12,16 @@ import java.util.List;
 @Slf4j
 public class Main {
     public static void main(String[] args) {
+        // Creación de elementos de la orden
         List<OrderItem> items = List.of(new OrderItem(101L, 1, 1200.00), new OrderItem(103L, 1, 89.90));
         ShippingAddress address = new ShippingAddress("Av. universitaria", "Nuevo Chimbote", "10001", "USA");
         PurchaseOrder order = new PurchaseOrder(1L, items, address);
 
+        // Cliente que inicia la validación
         OrderProcessingClient client = new OrderProcessingClient();
         ValidationResult result = client.processOrder(order);
+
+        // Resultado global de la validación
         log.info("Resultado final: {}", result.isValid());
     }
 }
