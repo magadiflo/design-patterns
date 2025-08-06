bf35340a0d1146a86719f7931ab462e40d4af948
Martín Díaz <magadiflo@gmail.com>
2025-08-06 18:05:27 -0500

    Java - patrones de comportamiento - chain of responsibility - order validation - doc 05

--- src/main/java/dev/magadiflo/patterns/plainjava/behavioral/chainofresponsibility/ordervalidation/validation/handler/OrderValidationHandler.java
+++ src/main/java/dev/magadiflo/patterns/plainjava/behavioral/chainofresponsibility/ordervalidation/validation/handler/OrderValidationHandler.java
@@ -3,15 +3,10 @@ package dev.magadiflo.patterns.plainjava.behavioral.chainofresponsibility.orderv
 import dev.magadiflo.patterns.plainjava.behavioral.chainofresponsibility.ordervalidation.dto.ValidationResult;
 import dev.magadiflo.patterns.plainjava.behavioral.chainofresponsibility.ordervalidation.model.PurchaseOrder;
 
-// ============== CHAIN OF RESPONSIBILITY PATTERN - EXACT UML STRUCTURE ==============
 public interface OrderValidationHandler {
-    /***
-     * Method para establecer el siguiente handler en la cadena
-     */
+    // Method para establecer el siguiente handler en la cadena
     OrderValidationHandler setNext(OrderValidationHandler handler);
 
-    /***
-     * Method principal para manejar solicitudes
-     */
+    // Method principal para manejar solicitudes
     ValidationResult handle(PurchaseOrder order);
 }
