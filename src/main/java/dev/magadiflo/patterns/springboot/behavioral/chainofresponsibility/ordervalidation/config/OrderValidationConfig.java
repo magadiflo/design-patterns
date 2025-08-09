75d4b0541a402d1c16516532ab06d8a5b39eeca3
Martín Díaz <magadiflo@gmail.com>
2025-08-08 23:41:31 -0500

    Spring Boot - patrones de comportamiento - chain of responsibility - order validation - impl 7

---
+++ src/main/java/dev/magadiflo/patterns/springboot/behavioral/chainofresponsibility/ordervalidation/config/OrderValidationConfig.java
@@ -0,0 +1,28 @@
+package dev.magadiflo.patterns.springboot.behavioral.chainofresponsibility.ordervalidation.config;
+
+import dev.magadiflo.patterns.springboot.behavioral.chainofresponsibility.ordervalidation.handler.AddressValidationHandler;
+import dev.magadiflo.patterns.springboot.behavioral.chainofresponsibility.ordervalidation.handler.BusinessHoursValidationHandler;
+import dev.magadiflo.patterns.springboot.behavioral.chainofresponsibility.ordervalidation.handler.CustomerValidationHandler;
+import dev.magadiflo.patterns.springboot.behavioral.chainofresponsibility.ordervalidation.handler.OrderValidationHandler;
+import dev.magadiflo.patterns.springboot.behavioral.chainofresponsibility.ordervalidation.handler.StockValidationHandler;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Slf4j
+@Configuration
+public class OrderValidationConfig {
+    @Bean
+    public OrderValidationHandler orderValidationHandler(CustomerValidationHandler customerValidator,
+                                                         StockValidationHandler stockValidator,
+                                                         AddressValidationHandler addressValidator,
+                                                         BusinessHoursValidationHandler hoursValidator) {
+        log.info("Configurando cadena de validación de órdenes...");
+        customerValidator
+                .setNext(stockValidator)
+                .setNext(addressValidator)
+                .setNext(hoursValidator);
+
+        return customerValidator;
+    }
+}
