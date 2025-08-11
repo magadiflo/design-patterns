2c760dbadee84df2713662674dcfd29de21f0b59
Martín Díaz <magadiflo@gmail.com>
2025-08-11 15:49:30 -0500

    Spring Boot - patrones de comportamiento - chain of responsibility - order validation - doc 02

--- src/main/java/dev/magadiflo/patterns/springboot/behavioral/chainofresponsibility/ordervalidation/dto/response/ErrorResponse.java
+++
@@ -1,8 +0,0 @@
-package dev.magadiflo.patterns.springboot.behavioral.chainofresponsibility.ordervalidation.dto.response;
-
-import java.time.LocalDateTime;
-
-public record ErrorResponse(String errorCode,
-                            String message,
-                            LocalDateTime timestamp) {
-}
