45cb9848104d5668014ee19954a5ef98931142c1
Martín Díaz <magadiflo@gmail.com>
2025-08-12 18:07:44 -0500

    Java - patrones creacionales - factory method - notifications - impl 02

--- src/main/java/dev/magadiflo/patterns/plainjava/creational/factorymethod/notification/factory/NotificationCreator.java
+++ src/main/java/dev/magadiflo/patterns/plainjava/creational/factorymethod/notification/factory/NotificationCreator.java
@@ -1,16 +1,12 @@
 package dev.magadiflo.patterns.plainjava.creational.factorymethod.notification.factory;
 
 import dev.magadiflo.patterns.plainjava.creational.factorymethod.notification.model.Notification;
-import dev.magadiflo.patterns.plainjava.creational.factorymethod.notification.model.NotificationChannel;
 import lombok.extern.slf4j.Slf4j;
 
 import java.util.Objects;
 
 @Slf4j
 public abstract class NotificationCreator {
-
-    // Factory Method - debe ser implementado por las subclases
-    public abstract Notification createNotification();
 
     public void processNotification(String message, String recipient) {
         // Usar el factory method para crear la notificación apropiada
@@ -34,10 +30,9 @@ public abstract class NotificationCreator {
         notification.send(message, recipient);
 
         // Lógica de negocio después del envío (logging, métricas, etc)
-        this.logNotification(notification.getChannel(), recipient);
+        log.info("Logged: {} notificación enviado a {}", notification.getChannel(), recipient);
     }
 
-    private void logNotification(NotificationChannel channel, String recipient) {
-        log.info("Logged: {} notificación enviado a {}", channel, recipient);
-    }
+    // Factory Method - debe ser implementado por las subclases
+    public abstract Notification createNotification();
 }
