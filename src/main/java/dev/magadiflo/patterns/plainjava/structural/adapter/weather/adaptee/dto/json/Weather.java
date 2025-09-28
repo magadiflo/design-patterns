94d021d9b873e63cc51741ad8707cece8bbffeac
Martín Díaz <magadiflo@gmail.com>
2025-09-27 19:50:37 -0500

    Java - patrones estructurales - adapter - weather - impl 10

---
+++ src/main/java/dev/magadiflo/patterns/plainjava/structural/adapter/weather/adaptee/dto/json/Weather.java
@@ -0,0 +1,7 @@
+package dev.magadiflo.patterns.plainjava.structural.adapter.weather.adaptee.dto.json;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+@JsonIgnoreProperties(ignoreUnknown = true)
+public record Weather(String description) {
+}
